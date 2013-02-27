3838fa8100f1b13c7aecf76832162cfa211c4256
Rich Loveland <loveland.richard@gmail.com>
2013-02-27 14:00:04 -0500

    Renamed all functions and variables with the `anx-' prefix.

--- anx-docgen.el
+++ anx-docgen.el
@@ -1,30 +1,30 @@
 ;;; ANX-Docgen
 
-(defvar *an-stack* nil
+(defvar *anx-stack* nil
   "The stack where we stash our second-pass field definitions.")
 
-(defun an-alistp (alist)
+(defun anx-alistp (alist)
   ;; Object -> Boolean
   "Determine if ALIST is an association list."
   (and (listp alist)
        (every #'consp alist)))
 
-(defun an-get-alist-keys (alist)
+(defun anx-get-alist-keys (alist)
   ;; Alist -> List
   "If ALIST is an association list, return a list of its keys."
-  (if (an-alistp alist)
+  (if (anx-alistp alist)
       (mapcar (lambda (elem)
 		(car elem))
 	      alist)
     nil))
 
-(defun an-array-of-alistsp (array)
+(defun anx-array-of-alistsp (array)
   ;; Object -> Boolean
   "Determine if ARRAY is an array of association lists."
   (and (arrayp array)
-       (an-alistp (elt array 0))))
+       (anx-alistp (elt array 0))))
 
-(defun an-assoc-val (key alist)
+(defun anx-assoc-val (key alist)
   ;; Symbol Alist -> Object
   "Given KEY and ALIST, return the value referenced by KEY.
 Unlike `assoc', this function does not return the entire
@@ -34,34 +34,34 @@ key-value pair."
 	(cdr result)
       result)))
 
-(defun an-get-random-alist (array-of-alists)
+(defun anx-get-random-alist (array-of-alists)
   ;; Array of Alists -> Alist
   "Given an ARRAY-OF-ALISTS, return a random association list."
   (let* ((len (length array-of-alists))
 	 (rand (random len)))
     (elt array-of-alists rand)))
 
-(defun an-stack-push (item)
+(defun anx-stack-push (item)
   ;; Item -> State!
-  "Push ITEM onto the global `*an-stack*'."
-  (push item *an-stack*))
+  "Push ITEM onto the global `*anx-stack*'."
+  (push item *anx-stack*))
 
-(defun an-stack-pop ()
+(defun anx-stack-pop ()
   ;; State!
-  "Pop an item off of the global `*an-stack*'."
-  (pop *an-stack*))
+  "Pop an item off of the global `*anx-stack*'."
+  (pop *anx-stack*))
 
-(defun an-stack-emptyp ()
+(defun anx-stack-emptyp ()
   ;; -> Boolean
-  "Check if the global `*an-stack*' is empty."
-  (null *an-stack*))
+  "Check if the global `*anx-stack*' is empty."
+  (null *anx-stack*))
 
-(defun an-clear-stack ()
+(defun anx-clear-stack ()
   ;; State!
-  "Clear the contents of the global `*an-stack*'."
-  (progn (setq *an-stack* nil)))
+  "Clear the contents of the global `*anx-stack*'."
+  (progn (setq *anx-stack* nil)))
 
-(defun an-translate-boolean (symbol)
+(defun anx-translate-boolean (symbol)
   ;; Symbol -> String
   "Given a SYMBOL with a `false' boolean value in JSON, return ``No''.
 Otherwise, return ``Yes''."
@@ -69,222 +69,222 @@ Otherwise, return ``Yes''."
       "No"
     "Yes"))
 
-(defun an-print-object-standard-fields (object)
+(defun anx-print-object-standard-fields (object)
   ;; Alist -> IO
-  (let* ((fields (an-list-object-standard-fields object)))
-    (format *an-table-row*
+  (let* ((fields (anx-list-object-standard-fields object)))
+    (format *anx-table-row*
 	    (pop fields)
 	    (pop fields)
 	    (pop fields)
 	    (pop fields))))
 
-(defun an-list-object-standard-fields (object)
+(defun anx-list-object-standard-fields (object)
   ;; Alist -> List
   (list
-   (an-assoc-val 'name object)
-   (an-assoc-val 'type object)
-   (an-translate-boolean (an-assoc-val 'sort_by object))
-   (an-translate-boolean (an-assoc-val 'filter_by object))))
+   (anx-assoc-val 'name object)
+   (anx-assoc-val 'type object)
+   (anx-translate-boolean (anx-assoc-val 'sort_by object))
+   (anx-translate-boolean (anx-assoc-val 'filter_by object))))
 
-(defun an-object-has-fieldsp (object)
+(defun anx-object-has-fieldsp (object)
   ;; Alist -> Boolean
   (if (assoc 'fields object)
       t
     nil))
 
-(defun an-save-fields-for-later (object)
+(defun anx-save-fields-for-later (object)
   ;; Alist -> State!
-  (let ((name (an-assoc-val 'name object)))
-    (an-stack-push (cons name (an-assoc-val 'fields object)))))
+  (let ((name (anx-assoc-val 'name object)))
+    (anx-stack-push (cons name (anx-assoc-val 'fields object)))))
 
-(defun an-process-object (object)
+(defun anx-process-object (object)
   ;; Alist -> IO State!
   (progn
-    (an-print-to-scratch-buffer
-     (an-print-object-standard-fields object))
-    (if (an-object-has-fieldsp object)
-	(an-save-fields-for-later object))))
+    (anx-print-to-scratch-buffer
+     (anx-print-object-standard-fields object))
+    (if (anx-object-has-fieldsp object)
+	(anx-save-fields-for-later object))))
 
-(defun an-process-objects (array-of-alists)
+(defun anx-process-objects (array-of-alists)
   ;; Array -> IO State!
-  (an-print-to-scratch-buffer
+  (anx-print-to-scratch-buffer
    (format "\nh4. JSON Fields\n\n"))
-  (an-print-to-scratch-buffer
-   (format *an-table-header*))
+  (anx-print-to-scratch-buffer
+   (format *anx-table-header*))
   (mapc (lambda (object)
-	  (an-process-object object))
+	  (anx-process-object object))
 	array-of-alists))
 
-(defvar *an-table-header*
+(defvar *anx-table-header*
   "|| Name || Type || Sort By? || Filter By? || Description || Default || Required On ||\n")
 
-(defvar *an-table-row*
+(defvar *anx-table-row*
   "| %s | %s | %s | %s | | | |\n")
 
-(defun an-print-to-scratch-buffer (format-string)
+(defun anx-print-to-scratch-buffer (format-string)
   (princ format-string
 	 (get-buffer "*scratch*")))
 
-(defun an-process-stack-item (list)
+(defun anx-process-stack-item (list)
   ;; List -> IO State!
   (let ((array-of-alists (cdr list))
 	(name (capitalize (car list))))
-    (an-print-to-scratch-buffer
+    (anx-print-to-scratch-buffer
      (format "\nh4. %s\n\n" name))
-    (an-print-to-scratch-buffer
-     (format *an-table-header*))
+    (anx-print-to-scratch-buffer
+     (format *anx-table-header*))
     (mapc (lambda (object)
 	    ;; Nothing should have fields at this level (I hope).
-	    (an-process-object object))
+	    (anx-process-object object))
 	  array-of-alists)))
 
-(defun an-process-stack-items ()
+(defun anx-process-stack-items ()
   ;; -> IO State!
-  (while (not (an-stack-emptyp))
-    (an-process-stack-item (an-stack-pop))))
+  (while (not (anx-stack-emptyp))
+    (anx-process-stack-item (anx-stack-pop))))
 
-(defun an-print-meta (array-of-alists)
+(defun anx-print-meta (array-of-alists)
   ;; Array -> IO State!
-  (if (an-array-of-alistsp array-of-alists)
+  (if (anx-array-of-alistsp array-of-alists)
       (progn
-	(an-clear-stack)
-	(an-process-objects array-of-alists)
-	(an-process-stack-items))
-    (error "`an-print-meta' expects an array of association lists.")))
+	(anx-clear-stack)
+	(anx-process-objects array-of-alists)
+	(anx-process-stack-items))
+    (error "`anx-print-meta' expects an array of association lists.")))
 
-(defun an-really-print-meta ()
+(defun anx-really-print-meta ()
   ;; -> IO State!
   (interactive)
   (let ((array-of-alists (read (buffer-string))))
-    (an-print-meta array-of-alists)))
+    (anx-print-meta array-of-alists)))
 
 ;;; Reporting.
 
-(defvar *an-dimensions-table-header*
+(defvar *anx-dimensions-table-header*
   "\n|| Column || Type || Filter? || Description ||\n")
 
-(defvar *an-metrics-table-header*
+(defvar *anx-metrics-table-header*
   "\n|| Column || Type || Formula || Description ||\n")
 
-(defvar *an-havings-hash* (make-hash-table :test 'equal)) ; -> Exists?
-(defvar *an-filters-hash* (make-hash-table :test 'equal)) ; -> Exists?
-(defvar *an-columns-hash* (make-hash-table :test 'equal)) ; -> Type
+(defvar *anx-havings-hash* (make-hash-table :test 'equal)) ; -> Exists?
+(defvar *anx-filters-hash* (make-hash-table :test 'equal)) ; -> Exists?
+(defvar *anx-columns-hash* (make-hash-table :test 'equal)) ; -> Type
 
-(defun an-build-columns-hash (report-meta-alist)
+(defun anx-build-columns-hash (report-meta-alist)
   ;; Array -> State!
   (mapc (lambda (alist) 
-	  (puthash (an-assoc-val 'column alist) 
-		   (an-assoc-val 'type alist) 
-		   *an-columns-hash*))
-	(an-assoc-val 'columns report-meta-alist)))
+	  (puthash (anx-assoc-val 'column alist) 
+		   (anx-assoc-val 'type alist) 
+		   *anx-columns-hash*))
+	(anx-assoc-val 'columns report-meta-alist)))
 
-(defun an-build-filters-hash (report-meta-alist)
+(defun anx-build-filters-hash (report-meta-alist)
   ;; Array -> State!
   (mapc (lambda (alist) 
-	  (puthash (an-assoc-val 'column alist) 
-		   (an-assoc-val 'type alist)
-		   *an-filters-hash*))
-	(an-assoc-val 'filters report-meta-alist)))
+	  (puthash (anx-assoc-val 'column alist) 
+		   (anx-assoc-val 'type alist)
+		   *anx-filters-hash*))
+	(anx-assoc-val 'filters report-meta-alist)))
 
-(defun an-build-havings-hash (report-meta-alist)
+(defun anx-build-havings-hash (report-meta-alist)
   ;; Array -> State!
   (mapc (lambda (alist) 
-	  (puthash (an-assoc-val 'column alist) t *an-havings-hash*))
-	(an-assoc-val 'havings report-meta-alist)))
+	  (puthash (anx-assoc-val 'column alist) t *anx-havings-hash*))
+	(anx-assoc-val 'havings report-meta-alist)))
 
-(defun an-print-column-standard-fields (alist)
+(defun anx-print-column-standard-fields (alist)
   ;; Alist -> IO
   (format "| %s | %s |\n"
-	  (an-assoc-val 'column alist)
-	  (an-assoc-val 'type alist)))
+	  (anx-assoc-val 'column alist)
+	  (anx-assoc-val 'type alist)))
 
-(defun an-build-dimensions-list ()
+(defun anx-build-dimensions-list ()
   ;; -> List
   (let ((results nil))
     (maphash (lambda (k v) 
-	       (unless (gethash k *an-havings-hash*)
+	       (unless (gethash k *anx-havings-hash*)
 		 (push k results)))
-	     *an-columns-hash*)
+	     *anx-columns-hash*)
     (reverse results)))
 
-(defun an-build-metrics-list ()
+(defun anx-build-metrics-list ()
   ;; -> List
   (let ((results nil))
     (maphash (lambda (k v) 
 		 (push k results))
-	     *an-havings-hash*)
+	     *anx-havings-hash*)
     (reverse results)))
 
-(defun an-build-time-intervals-list (report-meta-alist)
+(defun anx-build-time-intervals-list (report-meta-alist)
   ;; Alist -> List
   (mapcar (lambda (x) x)
-	  (an-assoc-val 'time_intervals network-meta)))
+	  (anx-assoc-val 'time_intervals network-meta)))
 
-(defun an-get-dimension-type (dimension)
+(defun anx-get-dimension-type (dimension)
   ;; String -> String
-  (if (an-dimensionp dimension)
-      (gethash dimension *an-columns-hash*)
+  (if (anx-dimensionp dimension)
+      (gethash dimension *anx-columns-hash*)
     (error (format "%s is not a dimension!" dimension))))
 
-(defun an-get-metric-type (metric)
+(defun anx-get-metric-type (metric)
   ;; String -> String
-  (if (an-metricp metric)
-      (gethash metric *an-havings-hash*)
+  (if (anx-metricp metric)
+      (gethash metric *anx-havings-hash*)
     (error (format "%s is not a metric!" metric))))
 
-(defun an-dimensionp (dimension)
+(defun anx-dimensionp (dimension)
   ;; String -> Boolean
-  (member dimension (an-build-dimensions-list)))
+  (member dimension (anx-build-dimensions-list)))
 
-(defun an-metricp (metric)
+(defun anx-metricp (metric)
   ;; String -> Boolean
-  (gethash metric *an-havings-hash*))
+  (gethash metric *anx-havings-hash*))
 
-(defun an-print-report-meta (report-meta)
+(defun anx-print-report-meta (report-meta)
   ;; Array -> IO State!
   (progn
-    (an-build-columns-hash report-meta)
-    (an-build-filters-hash report-meta)
-    (an-build-havings-hash report-meta)
-    ;; (an-print-time-intervals report-meta)
-    (an-print-dimensions-table)
-    (an-print-metrics-table)
-    (an-clear-hashes)))
+    (anx-build-columns-hash report-meta)
+    (anx-build-filters-hash report-meta)
+    (anx-build-havings-hash report-meta)
+    ;; (anx-print-time-intervals report-meta)
+    (anx-print-dimensions-table)
+    (anx-print-metrics-table)
+    (anx-clear-hashes)))
 
-(defun an-really-print-report-meta ()
+(defun anx-really-print-report-meta ()
   ;; -> IO State!
   (interactive)
   (let ((report-meta (read (buffer-string))))
-    (an-print-report-meta report-meta)))
+    (anx-print-report-meta report-meta)))
 
-(defun an-print-time-intervals (report-meta)
-  (an-build-time-intervals-list report-meta))
+(defun anx-print-time-intervals (report-meta)
+  (anx-build-time-intervals-list report-meta))
 
-(defun an-print-dimensions-table ()
+(defun anx-print-dimensions-table ()
   ;; Array -> IO State!
   (progn 
-    (an-print-to-scratch-buffer *an-dimensions-table-header*)
+    (anx-print-to-scratch-buffer *anx-dimensions-table-header*)
     (mapcar (lambda (elem)
-	      (an-print-to-scratch-buffer (format "| %s | %s | %s | |\n" elem 
-						  (gethash elem *an-columns-hash*)
-						  (if (gethash elem *an-filters-hash*)
+	      (anx-print-to-scratch-buffer (format "| %s | %s | %s | |\n" elem 
+						  (gethash elem *anx-columns-hash*)
+						  (if (gethash elem *anx-filters-hash*)
 						      "Yes"
 						    "No"))))
-	    (an-build-dimensions-list))))
+	    (anx-build-dimensions-list))))
 
-(defun an-print-metrics-table ()
+(defun anx-print-metrics-table ()
   ;; Array -> IO State!
   (progn
-    (an-print-to-scratch-buffer *an-metrics-table-header*)
+    (anx-print-to-scratch-buffer *anx-metrics-table-header*)
     (mapcar (lambda (elem)
-	      (an-print-to-scratch-buffer (format "| %s | %s | | |\n" elem (gethash elem *an-columns-hash*))))
-	    (an-build-metrics-list))))
+	      (anx-print-to-scratch-buffer (format "| %s | %s | | |\n" elem (gethash elem *anx-columns-hash*))))
+	    (anx-build-metrics-list))))
 
-(defun an-clear-hashes ()
+(defun anx-clear-hashes ()
   ;; -> State!
-  (progn (clrhash *an-havings-hash*)
-	 (clrhash *an-columns-hash*)
-	 (clrhash *an-filters-hash*)))
+  (progn (clrhash *anx-havings-hash*)
+	 (clrhash *anx-columns-hash*)
+	 (clrhash *anx-filters-hash*)))
 
 ;;; Working with existing documentation
 
